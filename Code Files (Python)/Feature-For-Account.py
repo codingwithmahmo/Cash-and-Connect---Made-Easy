dc010cb68014a444258b83eb4a8e8799964f342c
Uzair Baig <baiguzair53@gmail.com>
2024-12-01 13:52:18 +0500

    Create Feature-For-Account.py

---
+++ Code Files (Python)/Feature-For-Account.py
@@ -0,0 +1,13 @@
+def modifyAccounts(self):
+        print("Account Number : ",self.accountNo)
+        self.name = input("Modify Account Holder Name : ")
+        self.deposit = int(input("Modify Balance : "))
+
+def getAccountsNo(self):
+        return self.accountNo
+
+def getAccountsHolderName(self):
+        return self.name
+
+def getAccountsType(self):
+        return self.type
